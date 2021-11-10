287c7200e2f34b92e543e03a7cd2f85f255d3288
Jessica Nelson <yahwehschica@yahoo.com>
2021-11-10 11:04:27 -0500

    commit tree 45efa1006eea5d9199a0c09eada0f821aa8171fa

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("")
+  reversedWord.toString()
+    if (word === reversedWord) {
+      return true 
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  // Find a way to test if a word is a palindrome. Does the first half match the last
+  half? Will first word match the word backwords, or reverse
 */
 
 /*
-  Add written explanation of your solution here
+  // if the word deeply equals the word in reverse, then isPalindrome 
+  returns true. else it will return false. If I split the string, reverse, then
+  join it will output what? 
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
